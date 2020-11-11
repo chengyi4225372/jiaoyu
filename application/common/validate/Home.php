199343eeb5bac072e2a2e35d28cf070bcaba5d54
longcaicheng <732345907@qq.com>
2020-11-11 17:49:24 +0800

    add

---
+++ application/common/validate/Home.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/11/11
+ * Time: 17:48
+ */
+namespace app\common\validate;
+
+
+use think\Validate;
+
+/**
+ * Class Home
+ * @package app\common\validate
+ * 前台验证器 基类
+ */
+class Home extends Validate
+{
+
+}
